1a0451f2079623998cb92f64c8cb664c9fac1297
matsutoba <matsubara_t@outlook.jp>
2025-11-08 18:43:12 +0900

    変数名の修正

--- backend/go/internal/crm/controllers/user_controller.go
+++ backend/go/internal/crm/controllers/user_controller.go
@@ -23,8 +23,8 @@ func NewUserController(s services.UserService) *UserController {
 	return &UserController{service: s}
 }
 
-func (cc *UserController) GetUsers(c *gin.Context) {
-	customers, err := cc.service.GetAllUsers()
+func (uc *UserController) GetUsers(c *gin.Context) {
+	customers, err := uc.service.GetAllUsers()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
